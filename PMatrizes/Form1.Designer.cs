cd223224d55cd7b2a06a76ffefaea42f6d7c0524
0030482411029 <0030482411029@LTI4103>
2024-10-29 09:09:48 -0300

    Adicionar arquivos de projeto.

---
+++ PMatrizes/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PMatrizes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExercicio1 = new System.Windows.Forms.Button();
+            this.btnExercicio2 = new System.Windows.Forms.Button();
+            this.btnExercicio3 = new System.Windows.Forms.Button();
+            this.btnExercicio4 = new System.Windows.Forms.Button();
+            this.btnExercicio5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExercicio1
+            // 
+            this.btnExercicio1.Location = new System.Drawing.Point(134, 33);
+            this.btnExercicio1.Name = "btnExercicio1";
+            this.btnExercicio1.Size = new System.Drawing.Size(196, 99);
+            this.btnExercicio1.TabIndex = 0;
+            this.btnExercicio1.Text = "Exercício 1";
+            this.btnExercicio1.UseVisualStyleBackColor = true;
+            this.btnExercicio1.Click += new System.EventHandler(this.btnExercicio1_Click);
+            // 
+            // btnExercicio2
+            // 
+            this.btnExercicio2.Location = new System.Drawing.Point(497, 33);
+            this.btnExercicio2.Name = "btnExercicio2";
+            this.btnExercicio2.Size = new System.Drawing.Size(196, 99);
+            this.btnExercicio2.TabIndex = 1;
+            this.btnExercicio2.Text = "Exercício 2";
+            this.btnExercicio2.UseVisualStyleBackColor = true;
+            this.btnExercicio2.Click += new System.EventHandler(this.btnExercicio2_Click);
+            // 
+            // btnExercicio3
+            // 
+            this.btnExercicio3.Location = new System.Drawing.Point(134, 198);
+            this.btnExercicio3.Name = "btnExercicio3";
+            this.btnExercicio3.Size = new System.Drawing.Size(196, 99);
+            this.btnExercicio3.TabIndex = 2;
+            this.btnExercicio3.Text = "Exercício 3";
+            this.btnExercicio3.UseVisualStyleBackColor = true;
+            this.btnExercicio3.Click += new System.EventHandler(this.btnExercicio3_Click);
+            // 
+            // btnExercicio4
+            // 
+            this.btnExercicio4.Location = new System.Drawing.Point(497, 198);
+            this.btnExercicio4.Name = "btnExercicio4";
+            this.btnExercicio4.Size = new System.Drawing.Size(196, 99);
+            this.btnExercicio4.TabIndex = 3;
+            this.btnExercicio4.Text = "Exercício 4";
+            this.btnExercicio4.UseVisualStyleBackColor = true;
+            // 
+            // btnExercicio5
+            // 
+            this.btnExercicio5.Location = new System.Drawing.Point(316, 339);
+            this.btnExercicio5.Name = "btnExercicio5";
+            this.btnExercicio5.Size = new System.Drawing.Size(196, 99);
+            this.btnExercicio5.TabIndex = 4;
+            this.btnExercicio5.Text = "Exercício 5";
+            this.btnExercicio5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExercicio5);
+            this.Controls.Add(this.btnExercicio4);
+            this.Controls.Add(this.btnExercicio3);
+            this.Controls.Add(this.btnExercicio2);
+            this.Controls.Add(this.btnExercicio1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExercicio1;
+        private System.Windows.Forms.Button btnExercicio2;
+        private System.Windows.Forms.Button btnExercicio3;
+        private System.Windows.Forms.Button btnExercicio4;
+        private System.Windows.Forms.Button btnExercicio5;
+    }
+}
+
---
+++ PMatrizes/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PMatrizes
+{
+    public partial class Form1 : Form
+    {
+        string auxiliar = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExercicio1_Click(object sender, EventArgs e)
+        {
+            int[] vetor = new int[20]; // sintaxe para criar vetor
+
+            for(int i = 0; i < 20; i++)
+            {
+                auxiliar = Interaction.InputBox("Digite um número", "Entrada de dados"); // para incluir entrar no menu Projeto
+                //> Adicionar Referencia
+                //> Selecionar microsoft.visualbasic.
+                //Depois adicionar nas bibliotecas 'using Microsoft.VisualBasic;'
+                // O primeiro parametro é o texto ao lado da caixa de entrada
+                // O segundo é p texto de cabeçalho
+
+                if(!int.TryParse(auxiliar, out vetor[i]))
+                {
+                    MessageBox.Show("Número inválido");
+                    i--; // decrementa um i para ler de novo 
+                }
+            }
+
+            Array.Reverse(vetor);
+            auxiliar = "";
+            foreach(int i in vetor)
+            {
+                auxiliar += i + "\n";
+            }
+
+            MessageBox.Show(auxiliar);
+        }
+
+        private void btnExercicio2_Click(object sender, EventArgs e)
+        {
+            string aux = "";
+
+            ArrayList lista = new ArrayList() // Sintaxe de criação de uma lista dinâmica. Necessario ter "using System.Collections
+            {
+                "Ana", "André", "Débora","Fátima", "João", "Janete", "Otávio", "Marcelo", "Pedro", "Thais"
+            };
+            lista.Remove("Otávio");
+
+            foreach(string i in lista)
+            {
+                aux += i + "\n";
+            }
+
+            MessageBox.Show(aux);
+        }
+
+        private void btnExercicio3_Click(object sender, EventArgs e)
+        {
+             media;
+            double[,] notas = new double[20, 3]; //Sintaxe para criação de matriz
+            for(int i = 0; i < 20; i++)// Para acessar os alunos
+            {
+                for(int j = 0; j < 3; j++)// Para acessar as notas
+                {
+                    auxiliar = "";
+                    auxiliar = Interaction.InputBox("Dígite a nota do Aluno", "Entrada das notas");
+
+                    if (!Double.TryParse(auxiliar, out notas[i, j]))
+                    {
+                        MessageBox.Show("Valor inválido");
+                    }
+                    if (notas[i,j] < 0 || notas[i,j] > 10)
+                    {
+                        MessageBox.Show("As notas precisam estar entre 0 e 10");
+                        j--;
+                    }
+
+
+                }
+            }
+        }
+    }
+}
